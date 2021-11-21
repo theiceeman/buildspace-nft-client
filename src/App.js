bfa04247e410d2b740a1221cf173d7d7b831a2e5
kelvin <okorieebube1@gmail.com>
2021-11-21 19:03:44 +0100

    Fixed the view-connection button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import twitterLogo from "./assets/twitter-logo.svg";
 import { ethers } from "ethers";
 import myEpicNft from "./utils/MyEpicNFT.json";
 
-
 const TWITTER_HANDLE = "iotkelvin";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
-    const CONTRACT_ADDRESS = '0x060996B147726ade4Fc20a7b2485b15F660b1835';
+const CONTRACT_ADDRESS = "0x060996B147726ade4Fc20a7b2485b15F660b1835";
 
-const App =  () => {
+const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   // const { chainId } = await providers.getNetwork();
   // const deployedNetwork = myEpicNft.networks[chainId];
@@ -34,8 +33,8 @@ const App =  () => {
       setCurrentAccount(account);
 
       // Setup listener! This is for the case where a user comes to our site
-          // and ALREADY had their wallet connected + authorized.
-          setupEventListener()
+      // and ALREADY had their wallet connected + authorized.
+      setupEventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -68,13 +67,13 @@ const App =  () => {
 
       // Setup listener! This is for the case where a user comes to our site
       // and connected their wallet for the first time.
-      setupEventListener() 
+      setupEventListener();
     } catch (error) {
       console.log(error);
     }
   };
 
-    // Setup our listener.
+  // Setup our listener.
   const setupEventListener = async () => {
     // Most of this looks the same as our function askContractToMintNft
     try {
@@ -84,34 +83,39 @@ const App =  () => {
         // Same stuff again
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
 
-        let chainId = await ethereum.request({ method: 'eth_chainId' });
-console.log("Connected to chain " + chainId);
+        let chainId = await ethereum.request({ method: "eth_chainId" });
+        console.log("Connected to chain " + chainId);
 
-// String, hex code of the chainId of the Rinkebey test network
-const rinkebyChainId = "0x4"; 
-if (chainId !== rinkebyChainId) {
-	alert("You are not connected to the Rinkeby Test Network!");
-}
+        // String, hex code of the chainId of the Rinkebey test network
+        const rinkebyChainId = "0x4";
+        if (chainId !== rinkebyChainId) {
+          alert("You are not connected to the Rinkeby Test Network!");
+        }
 
         // THIS IS THE MAGIC SAUCE.
         // This will essentially "capture" our event when our contract throws it.
         // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
         });
 
-        console.log("Setup event listener!")
-
+        console.log("Setup event listener!");
       } else {
         console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const askContractToMintNft = async () => {
     try {
@@ -168,16 +172,15 @@ if (chainId !== rinkebyChainId) {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          <div style={
-    {height: 'fit-content'}} className="container m-auto">
-            <a
+          <div style={{ height: "fit-content" }} className="container m-auto">
+            <a style={{ paddingTop: "20px" }}
               href="https://testnets.opensea.io/collection/dinky-tales-v2"
               className="cta-button connect-wallet-button"
             >
-              🌊 View Collection on OpenSea 
+              🌊 View Collection on OpenSea
             </a>
           </div>
-          <br/>
+          <br />
           {currentAccount === "" ? (
             renderNotConnectedContainer()
           ) : (
